71a9fbf9d291538c39ba425ed9c8dc53ca8c4290
Ren0426 <144056243+Ren0426@users.noreply.github.com>
2025-03-01 12:02:57 -0500

    Update forms.py

--- finances/forms.py
+++ finances/forms.py
@@ -4,9 +4,10 @@ from .models import Income, Expenditure
 class IncomeForm(forms.ModelForm):
     class Meta:
         model = Income
-        fields = ['description', 'amount', 'date']
+        fields = ['source', 'amount', 'date']
 
 class ExpenditureForm(forms.ModelForm):
     class Meta:
         model = Expenditure
-        fields = ['description', 'amount', 'date']
+        fields = ['category', 'amount', 'date']
+        
